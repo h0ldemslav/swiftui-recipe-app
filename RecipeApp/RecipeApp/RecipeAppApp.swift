3bc43820e85060f4a159c41a12950c6a4f58fa64
Daniil Astapenko <xastapen@evo07.mendelu.cz>
2023-05-03 17:18:04 +0200

    Initial commit

---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Daniil Astapenko on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
